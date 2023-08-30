b8ab0f44b902569828910f51ca405cbebeac5629
huanghao <huanghao@youxin.cloud>
2023-08-30 19:33:16 +0800

    add login form validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,32 +2,71 @@
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
-import { ref } from 'vue';
+import { useField, useForm } from 'vee-validate';
 
-const username = ref('');
-const password = ref('');
+const { handleSubmit } = useForm();
+const { value: username, errorMessage: usernameInvalid } = useField<string>(
+  'username',
+  validateUsername,
+  { initialValue: '' },
+);
+const { value: password, errorMessage: passwordInvalid } = useField<string>(
+  'password',
+  validatePassword,
+  { initialValue: '' },
+);
 
-function onSubmit() {
-  console.log(`form submit! username={${username.value}} password={${password.value}}`);
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2));
+});
+
+function validateUsername(value: string) {
+  if (!value) {
+    return 'Username is required.';
+  }
+
+  return true;
+}
+
+function validatePassword(value: string) {
+  if (!value) {
+    return 'Password is required.';
+  }
+
+  return true;
 }
 </script>
 
 <template>
-  <form @submit.prevent="onSubmit" class="main flex flex-column justify-content-center gap-5 mt-5">
-    <span class="p-float-label">
-      <InputText class="w-full" id="username" v-model.trim="username" />
-      <label for="username">Username</label>
-    </span>
-    <span class="p-float-label">
-      <Password
-        inputClass="w-full"
-        inputId="password"
-        toggleMask
-        :feedback="false"
-        v-model="password"
-      />
-      <label for="password">Password</label>
-    </span>
+  <form @submit="onSubmit" class="main flex flex-column justify-content-center gap-3 mt-5">
+    <div>
+      <span class="p-float-label">
+        <InputText
+          class="w-full"
+          :class="{ 'p-invalid': usernameInvalid }"
+          id="username"
+          v-model.trim="username"
+        />
+        <label for="username">Username</label>
+      </span>
+      <small class="p-error">{{ usernameInvalid || '&nbsp;' }}</small>
+    </div>
+
+    <div>
+      <span class="p-float-label">
+        <Password
+          inputClass="w-full"
+          :class="{ 'p-invalid': passwordInvalid }"
+          inputId="password"
+          toggleMask
+          :feedback="false"
+          v-model="password"
+        />
+        <label for="password">Password</label>
+      </span>
+      <small class="p-error">{{ passwordInvalid || '&nbsp;' }}</small>
+    </div>
+
     <Button class="w-full" type="submit" label="Login" />
   </form>
 </template>
