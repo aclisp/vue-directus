81de913887e314d28f87c3c734b05fe20159d9c4
huanghao <huanghao@youxin.cloud>
2023-09-03 18:24:34 +0800

    修改文案

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Toast from 'primevue/toast';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Easy Chat" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
