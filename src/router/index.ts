1632ddadb2308845120f9865486ccbbf48cad183
huanghao <huanghao@youxin.cloud>
2023-10-20 15:14:51 +0800

    调整项目的结构：重命名目录

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView/LoginView.vue'),
+      component: () => import('../views/login/LoginView.vue'),
     },
     {
       path: '/about',
